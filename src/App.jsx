496bcd163c7a7e2cdac43bc6df02ba99f3374f2f
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2024-02-17 21:04:37 +0530

    make navbar and menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import "./App.css";
+import MainBox from "./MainBox";
 
-function App() {
-    return <h1>StreamEasy</h1>;
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
+
+export default function App() {
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <MainBox />
+        </ThemeProvider>
+    );
 }
-
-export default App;
---
+++ src/MainBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./MainBox.css";
+import PersistentDrawerLeft from "./PersistentDrawerLeft";
+
+export default function MainBox() {
+    return (
+        <div className="MainBox">
+            <PersistentDrawerLeft />
+        </div>
+    );
+}
